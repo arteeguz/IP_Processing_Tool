959bdf4a2b6722222c840f15b1b9ca84614271c2
arteeguz <arteeguz@gmail.com>
2024-08-18 23:56:25 -0400

    Updated Logger

--- IP_Processing_Tool/Logger.cs
+++ IP_Processing_Tool/Logger.cs
@@ -1,31 +1,39 @@
 ﻿using System;
 using System.IO;
+using System.Security.Principal;
 
 namespace IPProcessingTool
 {
     public static class Logger
     {
+        // private static readonly string LogFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "app.log");
         private static readonly string logFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "log.txt");
         // private static readonly string logFilePath = @"\\netapp2b\DSS Interns\IP_Scanner\log.txt";
 
-        public static void Log(LogLevel level, string message, string user = "System", string context = "", string additionalInfo = "")
+        public static void Log(LogLevel level, string message, string context = "", string additionalInfo = "")
         {
-            string logEntry = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} [{level}] User: {user}, Context: {context}, Additional Info: {additionalInfo}, Message: {message}";
-            WriteLog(logEntry);
-        }
+            string username = GetCurrentUsername();
+            string logEntry = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} [{level}] User: {username}, Context: {context}, Additional Info: {additionalInfo}, Message: {message}";
 
-        private static void WriteLog(string logEntry)
-        {
             try
             {
-                using (var writer = new StreamWriter(logFilePath, true))
-                {
-                    writer.WriteLine(logEntry);
-                }
+                File.AppendAllText(logFilePath, logEntry + Environment.NewLine);
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Failed to write log: {ex.Message}");
+                Console.WriteLine($"Failed to write to log file: {ex.Message}");
+            }
+        }
+
+        private static string GetCurrentUsername()
+        {
+            try
+            {
+                return WindowsIdentity.GetCurrent().Name;
+            }
+            catch (Exception)
+            {
+                return "Unknown";
             }
         }
     }
